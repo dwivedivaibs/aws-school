a73fe0afbfa3954c0c4ca73f4ad78a57a530baea
Vaibhav Dwivedi <vaibhav.dwivedi@clecotech.com>
2022-02-20 19:06:13 +0530

    update code for assets compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,9 +26,14 @@ Rails.application.configure do
   # config.assets.js_compressor = :uglifier
   config.assets.js_compressor = Uglifier.new(:harmony => true)
   # config.assets.css_compressor = :sass
+  config.assets.compress = true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+
+  config.serve_static_assets = false
+
+  config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
